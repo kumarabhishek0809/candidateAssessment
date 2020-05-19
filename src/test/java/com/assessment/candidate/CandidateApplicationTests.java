7975e3c841061f6f493ce29f6e091f2c002001df
kumar abhishek <mail2kumar.2021@gmail.com>
2020-05-19 10:57:11 +0400

    first commit

---
+++ src/test/java/com/assessment/candidate/CandidateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.candidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
