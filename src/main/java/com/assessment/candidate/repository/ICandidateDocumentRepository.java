86268032cbe8d37119802bd3e88c4e3cd99fd4a7
kumar abhishek <mail2kumar.2021@gmail.com>
2020-08-09 12:33:14 +0400

    http://localhost:8080/candidateDocument/search/emailId?emailId=kumar@gmail.com

--- src/main/java/com/assessment/candidate/repository/ICandidateDocumentRepository.java
+++ src/main/java/com/assessment/candidate/repository/ICandidateDocumentRepository.java
@@ -3,8 +3,14 @@ package com.assessment.candidate.repository;
 import com.assessment.candidate.entity.CandidateDocument;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 
 @RepositoryRestResource(collectionResourceRel = "candidateDocument", path = "candidateDocument")
 public interface ICandidateDocumentRepository extends CrudRepository<CandidateDocument, Integer> {
+
+    @RestResource(path = "emailId")
+    List<CandidateDocument> findByEmailId(String emailId);
 }
